a359c3bb91e98640219c83ce5667aad508409da9
yuncliu <lyc1990@qq.com>
2016-02-23 15:42:52 +0800

    refactor unittest

--- unittest/ast_test.cc
+++ unittest/ast_test.cc
@@ -8,114 +8,90 @@ Module* Single::m = NULL;
 IRBuilder<>* Single::b = NULL;
 std::map<std::string, Value*> Single::globalNamedValue;
 
-TEST(main, return0) {
-    Single::ReSet();
-    Single::getModule();
-    Single::getBuilder();
+class ASTTest: public testing::Test {
+    protected:
+        ExecutionEngine* EE;
+        virtual void SetUp() {
+            Single::ReSet();
+            Single::getModule();
+            Single::getBuilder();
+            EE = NULL;
+        }
 
-    yy_scan_string("int main() {"
-                       "return 10;"
-                   "}");
-    yyparse();
-    Single::getModule()->dump();
-    ExecutionEngine* EE = EngineBuilder(std::unique_ptr<Module>(Single::getModule())).create();
-    Function* Func = Single::getModule()->getFunction("main");
-    std::vector<GenericValue> noargs;
-    GenericValue gv = EE->runFunction(Func, noargs);
-    printf("Return value is [%ld]\n", *(gv.IntVal.getRawData()));
-    EXPECT_EQ(*(gv.IntVal.getRawData()), 10);
-    delete EE;
-}
+        int exe() {
+            EE = EngineBuilder(std::unique_ptr<Module>(Single::getModule())).create();
+            Function* Func = Single::getModule()->getFunction("main");
+            std::vector<GenericValue> noargs;
+            GenericValue gv = EE->runFunction(Func, noargs);
+            printf("Return value is [%ld]\n", *(gv.IntVal.getRawData()));
+            return *(gv.IntVal.getRawData());
+        }
 
-TEST(main, return_variable) {
-    Single::ReSet();
-    Single::getModule();
-    Single::getBuilder();
+        virtual void TearDown() {
+            if (NULL != EE) {
+                delete EE;
+                EE = NULL;
+            }
+        }
+};
 
-    yy_scan_string("int main() {"
-                       "int a = 10;"
-                       "return a;"
-                   "}");
-    yyparse();
-    Single::getModule()->dump();
-    ExecutionEngine* EE = EngineBuilder(std::unique_ptr<Module>(Single::getModule())).create();
-    Function* Func = Single::getModule()->getFunction("main");
-    std::vector<GenericValue> noargs;
-    GenericValue gv = EE->runFunction(Func, noargs);
-    printf("Return value is [%ld]\n", *(gv.IntVal.getRawData()));
-    EXPECT_EQ(*(gv.IntVal.getRawData()), 10);
-    delete EE;
-}
-
-TEST(main, return_global_variable) {
-    Single::ReSet();
-    Single::getModule();
-    Single::getBuilder();
-
-    yy_scan_string("int a = 10;"
-                   "int main() {"
-                   "    return a;"
-                   "}");
-    yyparse();
-    Single::getModule()->dump();
-    ExecutionEngine* EE = EngineBuilder(std::unique_ptr<Module>(Single::getModule())).create();
-    Function* Func = Single::getModule()->getFunction("main");
-    std::vector<GenericValue> noargs;
-    GenericValue gv = EE->runFunction(Func, noargs);
-    printf("Return value is [%ld]\n", *(gv.IntVal.getRawData()));
-    EXPECT_EQ(*(gv.IntVal.getRawData()), 10);
-    delete EE;
-}
-
-TEST(main, return_function) {
-    Single::ReSet();
-    Single::getModule();
-    Single::getBuilder();
+TEST_F(ASTTest, return0) {
 
     yy_scan_string("\
+            int main() {\
+            return 10;\
+            }");
+    yyparse();
+    Single::getModule()->dump();
+    EXPECT_EQ(this->exe(), 10);
+}
+
+TEST_F(ASTTest, return_variable) {
+    yy_scan_string("int main() {\
+            int a = 10;\
+            return a;\
+            }");
+yyparse();
+Single::getModule()->dump();
+EXPECT_EQ(this->exe(), 10);
+}
+
+TEST_F(ASTTest, return_global_variable) {
+    yy_scan_string("int a = 10;\
+            int main() {\
+            return a;\
+            }");
+    yyparse();
+    Single::getModule()->dump();
+    EXPECT_EQ(this->exe(), 10);
+}
+
+TEST_F(ASTTest, return_function) {
+    yy_scan_string("\
             int foo (int a) {\
-                a = 10;\
-                return a+1;\
+            a = 10;\
+            return a+1;\
             }\
             int main()\
             {\
-                return foo(1);\
+            return foo(1);\
             }");
 
     yyparse();
     Single::getModule()->dump();
-    ExecutionEngine* EE = EngineBuilder(std::unique_ptr<Module>(Single::getModule())).create();
-    Function* Func = Single::getModule()->getFunction("main");
-    std::vector<GenericValue> noargs;
-    GenericValue gv = EE->runFunction(Func, noargs);
-    printf("Return value is [%ld]\n", *(gv.IntVal.getRawData()));
-    EXPECT_EQ(*(gv.IntVal.getRawData()), 11);
-    delete EE;
-    Single::ReSet();
+    EXPECT_EQ(this->exe(), 11);
 }
 
-TEST(main, if_else) {
-    Single::ReSet();
-    Single::getModule();
-    Single::getBuilder();
-
+TEST_F(ASTTest, if_else) {
     yy_scan_string("\
-int main() {\
-    if (0) {\
-        return 0;\
-    }else {\
-        return 1;\
-    }\
-}");
-
+            int main() {\
+            if (0) {\
+            return 0;\
+            }else {\
+            return 1;\
+            }\
+            }");
     yyparse();
     Single::getModule()->dump();
-    ExecutionEngine* EE = EngineBuilder(std::unique_ptr<Module>(Single::getModule())).create();
-    Function* Func = Single::getModule()->getFunction("main");
-    std::vector<GenericValue> noargs;
-    GenericValue gv = EE->runFunction(Func, noargs);
-    printf("Return value is [%ld]\n", *(gv.IntVal.getRawData()));
-    EXPECT_EQ(*(gv.IntVal.getRawData()), 1);
-    delete EE;
-    Single::ReSet();
+    EXPECT_EQ(this->exe(), 1);
 }
